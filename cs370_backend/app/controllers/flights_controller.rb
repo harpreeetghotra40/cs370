a14d22a8db2909248b5669d3a27c2c9b029a57a0
Jack <jackinclass@gmail.com>
2019-12-02 07:03:13 +0800

    fetching flights data setup working

--- cs370_backend/app/controllers/flights_controller.rb
+++ cs370_backend/app/controllers/flights_controller.rb
@@ -1,9 +1,12 @@
 class FlightsController < ApplicationController
+  skip_before_action :authorized, only: [:index]
   def index
-    airportSource = Airport.find_by(:name => params[:airportSource]).id
-    airportDestination = Airport.find_by(:name => params[:airportDestination]).id
+    # byebug
+    airportSource = Airport.find_by(:location => params[:airportSource])
+    airportDestination = Airport.find_by(:location => params[:airportDestination])
+    date = DateTime.strptime(params[:date], "%Y-%m-%d")
     flights = Flight.all.select do |flight|
-      flight.airportSource == airpordSource && flight.airportDestination == airportDestination
+      flight.airportSource == airportSource.id && flight.airportDestination == airportDestination.id && ((flight.timeOfDeparture - date) > 0)
     end
     render json: flights.as_json(
       except: [:id, :updated_at, :created_at],
---
+++ cs370_frontend/js/index.js
@@ -0,0 +1,69 @@
+function checkLogin() {
+    if (localStorage.jwt === null || localStorage.jwt === undefined) {
+        location.href = 'login.html';
+    } else {
+        document.querySelector('#username').innerText = localStorage.username;
+    }
+}
+checkLogin();
+
+function makeReservation(event) {
+    event.preventDefault();
+    let flyCityA_round = document.querySelector("#flyCityA_round").value
+    let flyCityA_oneWay = document.querySelector("#flyCityA_oneWay").value
+    let flyCityB_round = document.querySelector("#flyCityB_round").value
+    let flyCityB_oneWay = document.querySelector("#flyCityB_oneWay").value
+    let flyingFromDate_round = document.querySelector("#flyingFromDate_round").value
+    let flyingFromDate_oneWay = document.querySelector("#flyingFromDate_oneWay").value
+    let flyingToDate_round = document.querySelector("#flyingToDate_round").value
+    let numOfPassenger = document.querySelector("#passenger").value
+    fetch("http://localhost:3000/flights", {
+            "method": "POST",
+            "headers": {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+            body: JSON.stringify({
+                "airportSource": flyCityA_oneWay,
+                "airportDestination": flyCityB_oneWay,
+                "date": flyingFromDate_oneWay
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            displayResults(response);
+        })
+}
+
+function clickArrow(event) {
+    event.target.click();
+}
+
+function logout() {
+    // console.log("halll");
+    localStorage.clear();
+    window.location.href = "login.html";
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent[0].style.display = "block";
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+function checkLogin() {
+    if (localStorage.jwt === null || localStorage.jwt === undefined) {
+        location.href = 'login.html';
+    } else {
+        document.querySelector('#username').innerText = localStorage.username;
+    }
+}
+checkLogin();
