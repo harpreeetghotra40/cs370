79b9fd630b7e0e0f090cbccc9cdb37d6bd65272a
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-12-05 15:20:11 -0500

    changes to db

--- cs370_backend/app/controllers/flights_controller.rb
+++ cs370_backend/app/controllers/flights_controller.rb
@@ -5,13 +5,25 @@ class FlightsController < ApplicationController
     airportSource = Airport.find_by(:location => params[:airportSource])
     airportDestination = Airport.find_by(:location => params[:airportDestination])
     date = params[:date].to_datetime
-    flights = Flight.all.select do |flight|
-      flight.airportSource == airportSource.id &&
-      flight.airportDestination == airportDestination.id &&
-      flight.timeOfDeparture.day == date.day &&
-      flight.timeOfDeparture.year == date.year &&
-      flight.timeOfDeparture.month == date.month
+    if (params[:airline] == null)
+      flights = Flight.all.select do |flight|
+        flight.airportSource == airportSource.id &&
+        flight.airportDestination == airportDestination.id &&
+        flight.timeOfDeparture.day == date.day &&
+        flight.timeOfDeparture.year == date.year &&
+        flight.timeOfDeparture.month == date.month
+      end
+    else
+      flights = Flight.all.select do |flight|
+        flight.airline.name == params[:airline]
+        flight.airportSource == airportSource.id &&
+        flight.airportDestination == airportDestination.id &&
+        flight.timeOfDeparture.day == date.day &&
+        flight.timeOfDeparture.year == date.year &&
+        flight.timeOfDeparture.month == date.month
+      end
     end
+
     render json: flights.as_json(
       except: [:updated_at, :created_at],
     )
